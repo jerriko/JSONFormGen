cf74d6b0d5ac18749d8d8f9783f029e15a0b6dfa
widobesh <jerriko64@gmail.com>
2019-08-25 08:58:57 -0500

    init generator functions

---
+++ formgen.js
@@ -0,0 +1,31 @@
+
+function readJSONTemplate(file) {
+	//Reads data structure in JSON template
+}
+
+function fieldToHTML(field) {
+	//Reads field parameters and returns html
+}
+
+function fieldJSValidation(field) {
+	//Reads field parameters and returns javascript validation
+}
+
+function appendHTML(html) {
+	//Appends field html to form
+}
+
+function appendJS(js) {
+	//Appends validation script to js
+}
+
+function generateForm(file) {
+	//returns HTML form
+}	
+
+function generateApp(file) {
+	//returns zipped node application containing:
+	//node server file
+	//html form
+	//javascript form validation
+}
